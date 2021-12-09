c88935318d4a135317496e0838825cc838097288
Ziobrowski Rafal <r-ziobrowski@wp.pl>
2021-12-09 14:38:06 +0100

    changed extensions of classes

--- lab06/tb_classes/env.svh
+++ lab06/tb_classes/env.svh
@@ -1,7 +1,7 @@
 class env extends uvm_env;
     `uvm_component_utils(env)
 
-    base_tester base_tester_h;
+    random_tester random_tester_h;
     driver driver_h;
     uvm_tlm_fifo #(ALU_input_t) command_f;
 
@@ -12,7 +12,7 @@ class env extends uvm_env;
 
     function void build_phase(uvm_phase phase);
         command_f         = new("command_f", this);
-        base_tester_h   = base_tester::type_id::create("base_tester_h",this);
+        random_tester_h   = random_tester::type_id::create("random_tester_h",this);
         driver_h          = driver::type_id::create("drive_h",this);
         coverage_h        = coverage::type_id::create ("coverage_h",this);
         scoreboard_h      = scoreboard::type_id::create("scoreboard_h",this);
@@ -22,7 +22,7 @@ class env extends uvm_env;
 
     function void connect_phase(uvm_phase phase);
         driver_h.command_port.connect(command_f.get_export);
-        base_tester_h.command_port.connect(command_f.put_export);
+        random_tester_h.command_port.connect(command_f.put_export);
         result_monitor_h.ap.connect(scoreboard_h.analysis_export);
         command_monitor_h.ap.connect(scoreboard_h.cmd_f.analysis_export);
         command_monitor_h.ap.connect(coverage_h.analysis_export);
